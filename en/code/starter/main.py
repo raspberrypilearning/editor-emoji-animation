6c709c3d0ca53f40e343748c6e51c31b2e2aba7f
Laura Sach <5183697+lawsie@users.noreply.github.com>
2024-06-21 09:17:57 +0100

    Now put what I wanted

--- en/code/starter/main.py
+++ en/code/starter/main.py
@@ -1,10 +1,38 @@
-# imports
+from p5 import *
+from random import randint
 
-# variables
-world = '🌍🌎🌏'
-python = 'Python 🐍'
-fire = '🔥'
+# Global variables
+seed_position = 0
+grow = 150
 
-# Function definitions        
-    
-# Put code to run under here
+def draw_background():
+    BROWN = Color(185, 155, 115)
+    BLUE = Color(130, 195, 255)
+    background(BLUE) 
+    fill(BROWN)
+    rect(0,300,400,100)
+
+def flower():
+    if seed_position >= 300:
+        text('🌱', 200, 300)    
+    if frame_count > grow: 
+        draw_background()
+        text('🌷', 200, 300)
+
+def sow_seeds():
+    global seed_position
+    if seed_position < 300:
+        seed_position = seed_position + 5
+        text('🫘', 200, seed_position)
+
+def setup():
+    size(400, 400)
+    no_stroke()
+    text_size(100)
+
+def draw(): 
+    draw_background()
+    flower()
+    sow_seeds()
+      
+run()
